2ef3c35804c25d4a1cab6fd7f3f2a02c42a9b035
Ahmad Ahandani <a.e.ahandani@gmail.com>
2025-05-11 23:21:20 -0700

    Enables cross-platform support
    Builds and packages binaries for both Windows and macOS.
    
    Updates the application to determine the correct binary path based on the operating system.
    
    Increases the package version to reflect these changes.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const path = require('path')
+const path = require('path');
+const os = require('os');
 
 module.exports = {
-    getBinaryPath: () => path.join(__dirname, 'textwrench-hotkeys.exe')
-}
+    getBinaryPath: () => {
+        const platform = os.platform();
+        if (platform === 'win32') {
+            return path.join(__dirname, 'textwrench-hotkeys.exe');
+        } else if (platform === 'darwin') {
+            return path.join(__dirname, 'textwrench-hotkeys-macos');
+        } else {
+            throw new Error(`Unsupported platform: ${platform}`);
+        }
+    }
+};
