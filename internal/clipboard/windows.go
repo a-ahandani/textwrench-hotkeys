93cae847b714180720608d36ea28726164315a60
Ahmad <a.e.ahandani@gmail.com>
2025-06-02 23:46:55 -0700

    Refactors app switching on Windows
    Replaces PowerShell script with `keybd_event` for switching
    to the next application on Windows. This improves
    reliability and avoids external process calls.

--- internal/clipboard/windows.go
+++ internal/clipboard/windows.go
@@ -4,8 +4,6 @@ package clipboard
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
 	"time"
 
 	"github.com/micmonay/keybd_event"
@@ -29,28 +27,18 @@ func sendKeyCombo(key int) error {
 }
 
 func switchToNextApp() error {
-	cmd := exec.Command("powershell", "-NoProfile", "-Command", `
-Add-Type -TypeDefinition @"
-using System;
-using System.Runtime.InteropServices;
-public class Keyboard {
-    [DllImport("user32.dll")]
-    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
-    const int KEYEVENTF_KEYUP = 0x0002;
-    const byte VK_MENU = 0x12;
-    const byte VK_TAB = 0x09;
+	kb, err := keybd_event.NewKeyBonding()
+	if err != nil {
+		return fmt.Errorf("failed to create key bonding: %w", err)
+	}
 
-    public static void SendAltTab() {
-        keybd_event(VK_MENU, 0, 0, UIntPtr.Zero);
-        keybd_event(VK_TAB, 0, 0, UIntPtr.Zero);
-        keybd_event(VK_TAB, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
-        keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
-    }
-}
-"@;
+	kb.SetKeys(keybd_event.VK_TAB)
+	kb.HasALT(true)
 
-[Keyboard]::SendAltTab();
-	`)
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	time.Sleep(100 * time.Millisecond)
+
+	if err := kb.Launching(); err != nil {
+		return fmt.Errorf("failed to switch to next app: %w", err)
+	}
+	return nil
 }
